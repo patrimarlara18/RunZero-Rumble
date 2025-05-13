89bab20d7dd586cebd9786553c36e224765ae296
patrimarlara18 <98734092+patrimarlara18@users.noreply.github.com>
2025-05-13 16:11:09 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -19,7 +19,7 @@ $logType = "RumbleAlerts"
 $timeGeneratedField = ""
 
 # Fetch the JSON content in the body of the HTTP POST request sent from Rumble via a webhook
-$obj = $request.Body -replace ',\s*([\]\}])', '$1'
+$obj = $request.Body
 Write-Host $obj
 
 Write-Host $obj.'changed_assets'
